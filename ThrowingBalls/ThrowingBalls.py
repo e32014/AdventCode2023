5dc8401161ccaa1187d0966e9e7333a0cf322d1c
Evan Klei <e32014@gmail.com>
2023-12-24 17:12:13 -0500

    Day 24: Reworking part 2 to use a brute force over a small range of velocities to find the right vector, which I thought was a pretty neat solution. does not work on the sample though.

--- ThrowingBalls/ThrowingBalls.py
+++ ThrowingBalls/ThrowingBalls.py
@@ -1,4 +1,5 @@
-from sympy import Symbol, solve_poly_system
+import itertools as it
+from fractions import Fraction
 
 file = open('input.txt')
 
@@ -8,23 +9,52 @@ for line in file:
     start, vel = line.strip().split("@")
     vectors.append(([int(i) for i in start.strip().split(", ")], [int(i) for i in vel.strip().split(", ")]))
 
-count = 0
-x = Symbol('x')
-y = Symbol('y')
-z = Symbol('z')
-dx = Symbol('dx')
-dy = Symbol('dy')
-dz = Symbol('dz')
-equations = []
-t_syms = []
-for i in range(3):
-    (x1, y1, z1), (dx1, dy1, dz1) = vectors[i]
-    t = Symbol(f"t{i}")
-    t_syms.append(t)
-    equations.append(x + dx * t - x1 - dx1 * t)
-    equations.append(y + dy * t - y1 - dy1 * t)
-    equations.append(z + dz * t - z1 - dz1 * t)
+pot_x = set(range(-1000, 1001))
+pot_y = set(range(-1000, 1001))
+pot_z = set(range(-1000, 1001))
 
-result = solve_poly_system(equations, *([x, y, z, dx, dy, dz] + t_syms))
-print(result)
-print(result[0][0] + result[0][1] + result[0][2])
+for v1, v2 in it.combinations(vectors, 2):
+    (x1, y1, z1), (dx1, dy1, dz1) = v1
+    (x2, y2, z2), (dx2, dy2, dz2) = v2
+
+    if dx1 == dx2:
+        new_x = set()
+        dist = x2 - x1
+        for v in range(-1000, 1001):
+            if v == dx1:
+                continue
+            if dist % (v - dx1) == 0:
+                new_x.add(v)
+        pot_x = pot_x & new_x
+    if dy1 == dy2:
+        new_y = set()
+        dist = y2 - y1
+        for v in range(-1000, 1001):
+            if v == dy1:
+                continue
+            if dist % (v - dy1) == 0:
+                new_y.add(v)
+        pot_y = pot_y & new_y
+    if dz1 == dz2:
+        new_z = set()
+        dist = z2 - z1
+        for v in range(-1000, 1001):
+            if v == dz1:
+                continue
+            if dist % (v - dz1) == 0:
+                new_z.add(v)
+        pot_z = pot_z & new_z
+
+r_dx, r_dy, r_dz = pot_x.pop(), pot_y.pop(), pot_z.pop()
+(x1, y1, z1), (dx1, dy1, dz1) = vectors[0]
+(x2, y2, z2), (dx2, dy2, dz2) = vectors[1]
+slope_1 = Fraction(dy1 - r_dy, dx1 - r_dx)
+slope_2 = Fraction(dy2 - r_dy, dx2 - r_dx)
+inter_1 = y1 - (slope_1 * x1)
+inter_2 = y2 - (slope_2 * x2)
+r_x = Fraction(inter_2 - inter_1, slope_1 - slope_2)
+r_y = slope_1 * r_x + inter_1
+time = Fraction(r_x - x1, dx1 - r_dx)
+r_z = z1 + (dz1 - r_dz) * time
+print(r_x, r_y, r_z)
+print(r_x + r_y + r_z)
